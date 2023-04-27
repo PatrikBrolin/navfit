7c9e44d4b751057c4c13382e0a7644589f410283
Patrik Brolin <patrikbrolin@hotmail.com>
2023-04-27 13:22:39 +0200

    image fix

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -5,18 +5,32 @@ import Image from "next/image";
 export default function Hero({ data }) {
   return (
     <section className={styles.heroWrapper}>
-      <img
+      <div className={styles.imageContainer}>
+        <Image
+          src={data?.items[0]?.backgrundsbild.url}
+          alt={data?.items[0]?.bildText}
+          width={100}
+          height={100}
+          layout="responsive"
+        />
+      </div>
+      {/* <img
         src={data?.items[0]?.backgrundsbild.url}
         alt={data?.items[0]?.bildText}
         className={styles.heroImg}
-      />
+      /> */}
       <div className={styles.textContent}>
         {/* <h2>{data?.items[0]?.rubrik}</h2> */}
-        <GeneratedTitle headline={data?.items[0]?.rubrik}/>
+        <GeneratedTitle headline={data?.items[0]?.rubrik} />
         <p>{data?.items[0]?.undertext}</p>
         <button className={styles.button}>
           <div className={cn(styles.buttonDesign)}>
-            <Image src="/icons/caret.png" alt="Triangel icon" height={30} width={30}/>
+            <Image
+              src="/icons/caret.png"
+              alt="Triangel icon"
+              height={30}
+              width={30}
+            />
           </div>
           {data?.items[0]?.callToActionKnapp}
         </button>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
 }
 
 module.exports = nextConfig
