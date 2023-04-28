db6950b08b5bcad5bd78c8961ba547fbaf92b952
Patrik Brolin <patrikbrolin@hotmail.com>
2023-04-28 14:10:16 +0200

    Adding lineSpace to long text

--- components/About/About.js
+++ components/About/About.js
@@ -3,6 +3,15 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import { useEffect, useState } from "react";
 export default function About({ data }) {
+
+  const renderOptions = {
+    renderText: text => {
+      return text.split('\n').reduce((children, textSegment, index) => {
+        return [...children, index > 0 && <br key={index} />, textSegment];
+      }, []);
+    },
+  };
+
   return (
     <div className={styles.aboutContainer}>
       <main>
@@ -12,7 +21,8 @@ export default function About({ data }) {
       </section>
       <section className={styles.aboutText}>
       <h2>Bakgrund:</h2>
-        {documentToReactComponents(data?.items[0]?.paragrafText?.json)}
+
+        {documentToReactComponents(data?.items[0]?.paragrafText?.json, renderOptions)}
       </section>
       <section className={styles.certificates}>
         <h2>Certifikat:</h2>
